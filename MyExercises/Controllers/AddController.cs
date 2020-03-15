759c0b017bf8b8e865ce75f00bbbba0d7980af0b
mpolox <mpolox@gmail.com>
2020-03-14 20:58:50 -0600

    First commit

---
+++ MyExercises/Controllers/AddController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyExercises.Models;
+
+namespace MyExercises.Controllers
+{
+    [ApiController]
+    [Route("add")]
+
+    public class AddController : Controller
+    {
+        private List<double> defaultList = new List<double>() { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        private readonly ILogger<WeatherForecastController> _logger;
+        public AddController(ILogger<AddController> logger)
+        {
+        }
+
+        [HttpPost]
+        public IActionResult Sum([FromBody]List<double> myNumbers)
+        {
+            if (myNumbers.Count < 2)
+            {
+                myNumbers = defaultList;
+            }
+            double result = 0;
+
+            
+            for (int i = 0; i < myNumbers.Count; i++)
+            {
+                result = result + myNumbers[i];
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ MyExercises/Models/IntegerList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyExercises.Models
+{
+    public class IntegerList
+    {
+        public List<int> Integers { get; set; }
+    }
+}
