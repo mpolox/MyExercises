35fe3aaf0446d1e5dc5b45f81987bea3cfa20247
mpolox <mpolox@gmail.com>
2020-03-16 17:53:13 -0600

    copy array

--- MyExercises/Controllers/AddController.cs
+++ MyExercises/Controllers/AddController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyExercises.Helpers;
 using MyExercises.Models;
 
 namespace MyExercises.Controllers
@@ -13,8 +14,12 @@ namespace MyExercises.Controllers
 
     public class AddController : Controller
     {
-        private List<double> defaultList = new List<double>() { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        private readonly List<double> defaultList = new List<double>() { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        private readonly List<int> defaultListIntegers = new List<int>() { 1,1,2,2,2,3,3,4 };
+        private ExampleHelper helper = new ExampleHelper();
+
         private readonly ILogger<AddController> _logger;
+
         public AddController(ILogger<AddController> logger)
         {
             _logger = logger;
@@ -28,13 +33,24 @@ namespace MyExercises.Controllers
                 myNumbers = defaultList;
             }
             double result = 0;
-
             
             for (int i = 0; i < myNumbers.Count; i++)
             {
-                result = result + myNumbers[i];
+                result += myNumbers[i];
             }
 
+            return Ok(result);
+        }
+
+        [HttpPost("arrayExample")]
+        public IActionResult ArrayExample02([FromBody] List<int> myNumbers)
+        {
+            if (myNumbers.Count < 2)
+            {
+                myNumbers = defaultListIntegers;
+            }
+            var numbers = myNumbers.ToArray();
+            var result = helper.ArrayExample01(ref numbers);
             return Ok(result);
         }
     }
---
+++ MyExercises/Helpers/ExampleHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyExercises.Helpers
+{
+    public class ExampleHelper
+    {
+        public int[] ArrayExample01(ref int[] numbers)
+        {
+            int latestItem = numbers[0];
+            bool duplicated = false;
+            int duplicatedCount = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (duplicated && (numbers[i] != latestItem))
+                {
+                    duplicated = false;
+                    latestItem = numbers[i];
+                }
+
+                if (duplicated && (numbers[i] == latestItem))
+                {                    
+                    numbers[i] = 0;
+                    duplicatedCount++;
+                }
+
+                if (!duplicated && (numbers[i] == latestItem))
+                {
+                    duplicated = true;                    
+                }
+            }
+
+            int[] newArray = new int[numbers.Length - duplicatedCount];
+            int newArrayCount = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    newArray[newArrayCount] = numbers[i];
+                    newArrayCount++;
+                }
+            }
+            return newArray;
+        }
+
+
+
+    }
+}
